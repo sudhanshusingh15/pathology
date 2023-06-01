5dc8ebc0a171fe27bafc550533fb439ab1a56f24
sudhanshusingh <ss4827@drexel.edu>
2023-06-01 19:21:38 -0400

    11th-commit

--- src/App.js
+++ src/App.js
@@ -4,26 +4,6 @@
 // import { HomePage } from './containers/HomePage';
 // import { CustomerAccessPage } from './containers/customerAccessPage';
 // import { UploadPage } from './containers/upload';
-// import { AboutUs } from './containers/about';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           <Route path="/user/access/:action" element={<CustomerAccessPage />} />
-//           <Route path="/upload" element={<UploadPage />} />
-//           <Route path="/about" element={<AboutUs />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
